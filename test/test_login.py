6d90c35fc99f3d7af2efbf26af25ddc8c849d52e
Singhoon <152477741+Singhoon@users.noreply.github.com>
2024-12-02 21:04:11 +0900

    Update test_login.py

--- test/test_login.py
+++ test/test_login.py
@@ -1,9 +1,8 @@
-import pytest
-from app import app
 import sys
 import os
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # 상위 디렉토리 추가
+from app import app
+import pytest
 
 @pytest.fixture
 def client():
